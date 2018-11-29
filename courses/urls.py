9fe6f54a247f35a84df53f1cd00a5420a65b3fd3
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-29 10:01:31 +0200

    course module update mixin view and url for it

--- courses/urls.py
+++ courses/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
          name='course_edit'),
     path('<pk>/delete/', views.CourseDeleteView.as_view(),
          name='course_delete'),
+    path('<pk>/module/', views.CourseModuleUpdateView.as_view(),
+         name='course_module_update'),
 ]
