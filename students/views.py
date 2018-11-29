70381416dd93a648d400b23821b2e886714cb9e7
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-29 20:07:37 +0200

    templates for courses and views for details

--- students/views.py
+++ students/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, FormView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CourseEnrollForm
+from courses.models import Course
 
 
 class StudentRegistrationView(CreateView):
@@ -35,3 +38,43 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
     def get_success_url(self):
         return reverse_lazy('student_course_detail',
                             args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super(StudentCourseListView, self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super(StudentCourseDetailView, self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentCourseDetailView,
+                        self).get_context_data(**kwargs)
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            context['module'] = course.modules.get(
+                id=self.kwargs['module_id'])
+        else:
+            context['module'] = course.modules.all()[0]
+        return context
+
+
+
+
+
+
+
+
+
+
+
