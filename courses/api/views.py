a0dd4522e0e837c413f73d943126240ac18c4876
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-12-02 16:29:47 +0200

    views with nested serializer fields

--- courses/api/views.py
+++ courses/api/views.py
@@ -8,6 +8,8 @@ from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from ..models import Subject, Course
 from .serializers import SubjectSerializer, CourseSerializer
+from .permissions import IsEnrolled
+from .serializers import CourseWithContentsSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -31,3 +33,10 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @detail_route(methods=['get'],
+                  serializer_class=CourseWithContentsSerializer,
+                  authentication_classes=[BasicAuthentication],
+                  permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
